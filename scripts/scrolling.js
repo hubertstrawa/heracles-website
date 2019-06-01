5951bb10fe5ccdc53a6fe898e6f24fb42f729d49
Vinyljam <hubertstrawa@gmail.com>
2019-06-01 02:06:26 +0200

    adding mobile menu and mq

---
+++ scripts/scrolling.js
@@ -0,0 +1,13 @@
+import smoothscroll from 'smoothscroll-polyfill';
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 850,
+    offset: -30,
+});   
+
+var scroll = new SmoothScroll('a[href*="#galeria"]', {
+    speed: 850,
+    offset: -35,
+});   
+
+export default scrolling;
